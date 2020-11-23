5d102b7160a11e325fcc278465c445b9aac0d96b
Yevheniia Dehtiarova <7145714-YevheniiaDehtiarova@users.noreply.gitlab.com>
2020-11-24 00:21:16 +0200

    4 part complete

---
+++ src/js/slider.js
@@ -0,0 +1,17 @@
+// $(function() {
+//     $('.coaches__slider').slick({
+//         prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
+//         nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
+//         infinite: false,
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//     });
+
+// });
+$(document).ready(function() {
+    $('.coaches__slider').slick();
+});
+// script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js " > < /script>
+
+// <
+// script src = "src/js/slick.min.js" > < /script>
