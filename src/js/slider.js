f9ec93266badf73d642a1ac73832dc7cbe433451
Yevheniia Dehtiarova <7145714-YevheniiaDehtiarova@users.noreply.gitlab.com>
2020-11-24 21:39:16 +0200

    update slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,60 @@
-$(function() {
-    $('.coaches__slider').slick({
-        prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
-        nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-    });
+// $(function() {
+//     $('.coaches__slider').slick({
+//         prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
+//         nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
+//         infinite: true,
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         responsive: [{
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1.5,
+//                 slidesToScroll: 1,
+//                 centerMode: true,
+//                 infinite: true,
+//                 dots: true,
+//             }
+//         }]
+//     });
 
+// });
+
+// $(function() {
+//     $('.coaches__slider').slick({
+//         prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
+//         nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
+//         infinite: true,
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         responsive: [{
+//             breakpoint: 640,
+//             settings: {
+//                 slidesToShow: 1.5,
+//                 slidesToScroll: 1,
+//                 centerMode: true,
+//                 infinite: true,
+//                 // dots: true,
+//             }
+//         }]
+//     });
+
+// });
+
+$('.coaches__slider').slick({
+    prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
+    nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [{
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1.5,
+            slidesToScroll: 1,
+            centerMode: true,
+            infinite: true,
+            dots: false,
+            centerPadding: '5px',
+        }
+    }]
 });
