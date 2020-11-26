7693eaa3a403b7c112ebf0d78b949af93633aba8
Yevheniia Dehtiarova <7145714-YevheniiaDehtiarova@users.noreply.gitlab.com>
2020-11-26 22:37:44 +0200

    update styles

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,45 +1,3 @@
-// $(function() {
-//     $('.coaches__slider').slick({
-//         prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
-//         nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
-//         infinite: true,
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         responsive: [{
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 1.5,
-//                 slidesToScroll: 1,
-//                 centerMode: true,
-//                 infinite: true,
-//                 dots: true,
-//             }
-//         }]
-//     });
-
-// });
-
-// $(function() {
-//     $('.coaches__slider').slick({
-//         prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
-//         nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
-//         infinite: true,
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         responsive: [{
-//             breakpoint: 640,
-//             settings: {
-//                 slidesToShow: 1.5,
-//                 slidesToScroll: 1,
-//                 centerMode: true,
-//                 infinite: true,
-//                 // dots: true,
-//             }
-//         }]
-//     });
-
-// });
-
 $('.coaches__slider').slick({
     prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
     nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
@@ -47,7 +5,7 @@ $('.coaches__slider').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
     responsive: [{
-        breakpoint: 600,
+        breakpoint: 640,
         settings: {
             slidesToShow: 1,
             slidesToScroll: 2,
