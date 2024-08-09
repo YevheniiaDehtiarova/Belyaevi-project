557aae05a006fe2f75581f9e651526494106133b
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-08-09 23:56:58 +0300

    create new sections charity and results

--- src/js/slider.js
+++ src/js/slider.js
@@ -16,3 +16,22 @@ $('.coaches__slider').slick({
         }
     }]
 });
+
+$('.news__slider').slick({
+    prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
+    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">', 
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 640,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+            centerMode: true,
+            variableWidth: true,
+        }
+    }]
+});
