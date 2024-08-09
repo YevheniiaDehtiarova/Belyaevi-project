8d09e52091e254655bc5b846e4a11e0d6298c0cd
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-08-10 00:02:54 +0300

    create faq session

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,6 @@
 
     };
 
-    // Табуляція
 const tabButtons = document.querySelectorAll('.tab-button');
 tabButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -22,23 +21,15 @@ tabButtons.forEach(button => {
     });
 });
 
-// Аккордеон
 const accordionItems = document.querySelectorAll('.accordion-item');
 accordionItems.forEach(item => {
     const title = item.querySelector('.accordion-title');
     title.addEventListener('click', () => {
         const content = item.querySelector('.accordion-content');
         const isOpen = content.style.display === 'block';
-        
-        // Закрити всі аккордеони
         document.querySelectorAll('.accordion-content').forEach(content => content.style.display = 'none');
-        
-        // Відкрити/Закрити поточний аккордеон
         content.style.display = isOpen ? 'none' : 'block';
     });
 });
 
 })();
-// script src = "https://code.jquery.com/jquery-3.5.1.js"
-// integrity = "sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-// crossorigin = "anonymous" > < /script>
