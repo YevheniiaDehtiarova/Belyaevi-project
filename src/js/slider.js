cb6df05dee4752d85048b38a030fb9a23610a318
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-08-26 11:33:41 +0300

    modify responsive slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,25 +1,31 @@
-$('.coaches__slider').slick({
-    prevArrow: ' <img src="src/assets/img/arrow-left.svg" alt="">',
-    nextArrow: ' <img src="src/assets/img/arrow-right.svg" alt="">',
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    responsive: [{
-        breakpoint: 640,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: false,
-            centerMode: true,
-            variableWidth: true,
-        }
-    }]
-});
+
 
 $('.news__slider').slick({
     prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
-    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">', 
+    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10px',
+    responsive: [{
+        breakpoint: 821,
+        settings: {
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            slidesToShow: 1,
+        }
+    }]
+});
+
+
+$('.volunteering__slider').slick({
+    prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
+    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -32,7 +38,6 @@ $('.news__slider').slick({
             dots: false,
             centerMode: true,
             variableWidth: true,
-            
         }
     },
     {
@@ -48,32 +53,7 @@ $('.news__slider').slick({
     }]
 });
 
-$('.volunteering__slider').slick({
-    prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
-    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">', 
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-        breakpoint: 640,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-            centerMode: true,
-            variableWidth: true,
-        }
-    },
-    {
-        breakpoint: 480,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false,
-            centerMode: true,
-            variableWidth: true,
-        }
-    }]
+
+$(window).on('resize', function() {
+    $('.news__slider').slick('setPosition');
 });
